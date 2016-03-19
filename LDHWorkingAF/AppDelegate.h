ca680897bb434fc9a107deddedf75c16a3ab02e9
Daredos <asiosldh@163.com>
2016-03-20 00:17:57 +0800

    Initial Commit

---
+++ LDHWorkingAF/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LDHWorkingAF
+//
+//  Created by Daredos on 16/3/20.
+//  Copyright © 2016年 LiangDaHong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LDHWorkingAF/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LDHWorkingAF
+//
+//  Created by Daredos on 16/3/20.
+//  Copyright © 2016年 LiangDaHong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
